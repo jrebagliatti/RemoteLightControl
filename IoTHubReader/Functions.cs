ab1be1c2dba298a9157cbf128ceb8b4d369c8cce
Javier Rebagliatti <jrebagliatti@southworks.com>
2016-09-27 19:15:06 -0300

    Added logic to read from the IoT Hub

--- IoTHubReader/Functions.cs
+++ IoTHubReader/Functions.cs
@@ -5,25 +5,44 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs;
+using Microsoft.ServiceBus.Messaging;
+using System.Threading;
 
 namespace IoTHubReader
 {
     public class Functions
     {
+        static string connectionString = "HostName=remotelightcontrol.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=liSFDuYnBXogLsPbPJVZMvo4plkl4AfrP73XSnooGjc=";
+        static string iotHubD2cEndpoint = "messages/events";
+        static EventHubClient eventHubClient;
+
         [NoAutomaticTriggerAttribute]
         public static async Task ProcessMethod()
         {
+            Console.Out.WriteLine("Stablishing IoT Hub connection.");
+            eventHubClient = EventHubClient.CreateFromConnectionString(connectionString, iotHubD2cEndpoint);
+            Console.Out.WriteLine("Connection to IoT Hub stablished.");
+
+            var d2cPartitions = eventHubClient.GetRuntimeInformation().PartitionIds;
+
+            var tasks = new List<Task>();
+            foreach (string partition in d2cPartitions)
+            {
+                tasks.Add(ReceiveMessagesFromDeviceAsync(partition));
+            }
+            Task.WaitAll(tasks.ToArray());
+        }
+
+        private static async Task ReceiveMessagesFromDeviceAsync(string partition)
+        {
+            var eventHubReceiver = eventHubClient.GetDefaultConsumerGroup().CreateReceiver(partition, DateTime.UtcNow);
             while (true)
             {
-                try
-                {
-                    Console.Out.WriteLine("ProcessMethod call");
-                }
-                catch (Exception ex)
-                {
-                    Console.Error.WriteLine("Error occurred in processing pending altapay requests. Error : {0}", ex.Message);
-                }
-                await Task.Delay(TimeSpan.FromMinutes(10));
+                EventData eventData = await eventHubReceiver.ReceiveAsync();
+                if (eventData == null) continue;
+
+                string data = Encoding.UTF8.GetString(eventData.GetBytes());
+                Console.WriteLine("Message received. Partition: {0} Data: '{1}'", partition, data);
             }
         }
     }
