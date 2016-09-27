6ac60ccdb5d2235e844a72fe862436694e2bfc57
Vivi <Vivi@PC_VI_JA>
2016-09-26 23:38:49 -0300

    Add project files.

---
+++ IoTHubReader/Functions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+
+namespace IoTHubReader
+{
+    public class Functions
+    {
+        [NoAutomaticTriggerAttribute]
+        public static async Task ProcessMethod(TextWriter log)
+        {
+            while (true)
+            {
+                try
+                {
+                    log.WriteLine("ProcessMethod call");
+                }
+                catch (Exception ex)
+                {
+                    log.WriteLine("Error occurred in processing pending altapay requests. Error : {0}", ex.Message);
+                }
+                await Task.Delay(TimeSpan.FromMinutes(10));
+            }
+        }
+    }
+}
